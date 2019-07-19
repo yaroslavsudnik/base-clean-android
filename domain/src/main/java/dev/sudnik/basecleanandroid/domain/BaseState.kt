3edcf79768c5520dc922240e168caf89e1f59d3d
Yaroslav Sudnik <yaroslav.sudnik@flatstack.com>
2019-07-19 15:46:04 +0300

    first commit

---
+++ domain/src/main/java/dev/sudnik/basecleanandroid/domain/BaseState.kt
@@ -0,0 +1,3 @@
+package dev.sudnik.basecleanandroid.domain
+
+sealed class BaseState
---
+++ presentation/src/main/java/dev/sudnik/basecleanandroid/presentation/BaseActivity.kt
@@ -0,0 +1,5 @@
+package dev.sudnik.basecleanandroid.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity()
